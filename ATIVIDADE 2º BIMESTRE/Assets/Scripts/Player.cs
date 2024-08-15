128a218ec1940b30f0a235f5cca8edc1c890f559
IthaloDavy <129884701+IthaloDavy@users.noreply.github.com>
2024-08-15 14:09:15 -0300

    Animator completo! Personagem anda, fica ocioso e pula normalmente.

--- ATIVIDADE 2º BIMESTRE/Assets/Scripts/Player.cs
+++ ATIVIDADE 2º BIMESTRE/Assets/Scripts/Player.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed;
-    
+
     public float JumpForce;
 
     private Rigidbody2D rig;
-    
+
+    private Animator anim;
+
     public bool isJumping;
-    
+
     public bool doubleJump;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,42 +32,64 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f,0f);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
     }
 
     void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
         {
-            if (!isJumping)
+            if (Input.GetButtonDown("Jump"))
             {
-                rig.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
-                doubleJump = true;
-            }
-            else
-            {
-                if (doubleJump)
+                if (!isJumping)
                 {
-                    rig.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
-                    doubleJump = false;
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = true;
+                    anim.SetBool("jump", true);
                 }
+                else
+                {
+                    if (doubleJump)
+                    {
+                        rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                        doubleJump = false;
+                    }
+                }
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == 8)
+            {
+                isJumping = false;
+                anim.SetBool("jump", false);
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == 8)
+            {
+                isJumping = true;
             }
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
-        {
-            isJumping = false;
-        }
-    }
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
-        {
-            isJumping = true;
-        }
-    }
-}
+
