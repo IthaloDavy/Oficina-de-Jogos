bcdba4419de69dfb2628df22099d5e48468bc2a1
IthaloDavy <129884701+IthaloDavy@users.noreply.github.com>
2024-08-22 14:07:31 -0300

    Tela de Game Over e Plataforma Flutuante.

--- ATIVIDADE 2º BIMESTRE/Assets/Scripts/GameController.cs
+++ ATIVIDADE 2º BIMESTRE/Assets/Scripts/GameController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public int totalScore;
 
     public TextMeshProUGUI scoreText;
+
+    public GameObject gameOver;
 
     public static GameController instance;
     // Start is called before the first frame update
@@ -20,6 +23,16 @@ public class GameController : MonoBehaviour
     public void UpdateScoreText()
     {
         scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
     }
     
 }
